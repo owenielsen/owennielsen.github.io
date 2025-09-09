db4efb4ecc0412a676e8bc0d73c8183b4701d803
owenielsen <owen.nielsen@malad.us>
2025-09-09 10:18:54 -0700

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+//This is my second program for Prog and Dev 1.4
+//Enjoy!
+//By the way this is by Owen Nielsen
+public class evenodd {
+    public static void main(String[] args){
+        // Loop from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            // Check if the number is even or odd 
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+    }
+}
