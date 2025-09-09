1e18312b4843803ef53da03bbbb7f023933fb00b
owenielsen <owen.nielsen@malad.us>
2025-09-09 09:08:20 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,14 @@
+//Owen Nielsen owen.nielsen@malad.us
+//This program is for assignment 1.3 and for you, the viewer
+//This is my first HelloWorld program!
+
 public class HelloWorld {
     public static void main(String[] args){
-
+        int age=15;        
         System.out.println("My name is Owen Nielsen");
+        System.out.println("I am "+age+" years old");
+        System.out.println("My brother's name is Bob.");
+        System.out.println("He has broken 8 bones and he is 10");//No, they weren't all my fault, he's just reckless
     }
 }
 
