be6766f02cf57a8aa2651c00eac688ef486a4eca
owenielsen <owen.nielsen@malad.us>
2026-03-23 10:35:07 -0700

    32326

---
+++ horse.java/errorKG/Main.java
@@ -0,0 +1,37 @@
+// error filled file created by KG
+//-Owen
+//capitalization errors, syntax mistakes, codes being placed in the wrong spots
+/*
+Error filled file created by
+Kimg@techtrepacademy.com
+for my java class to learn troubleshooting on
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello everybody");
+        System.out.println("am I doing this correctly");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the number i is " + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the new number i is " + i);
+        }
+
+        int i = 0; // example value for if-check
+        if (i == 0) {
+            System.out.print("i is equal to zero");
+        } else {
+            System.out.print("i is not equal to zero");
+        }
+
+        // what other errors can I put into a source code file?
+        // - missing semicolon
+        // - wrong case on identifiers (System vs system)
+        // - invalid loop syntax (for (Int i=0:i>10;i++) )
+        // - calling methods outside a method body
+        // - unclosed braces, unmatched parens
+    }
+}
